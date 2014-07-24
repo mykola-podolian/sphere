49097087abd7347c11a54b2d2721ad51f0629653
mykola-podolian <mykola.podolian@globallogic.com>
2014-07-24 10:01:05 +0300

    added cube

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,83 @@
 #define window_width  640
 #define window_height 480
 
-
-
 // Main loop
 void main_loop_function() {
+	static float angle = 0;
 	// And depth (used internally to block obstructed objects)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	// Load identity matrix
+	glLoadIdentity();
 
+	glTranslatef(0, 0, -7);
+	glRotatef(angle, 1, 1, 0);
+
+	glBegin(GL_TRIANGLES);
+
+	glColor3f(255, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+
+	glColor3f(0, 255, 0);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(1, 1, 1);
+
+	glColor3f(255, 255, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(1, -1, -1);
+
+	glColor3f(0, 0, 255);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+
+	glColor3f(255, 255, 255);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+
+	glColor3f(255, 0, 255);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glEnd();
 	glutSwapBuffers();
+	angle += 2;
+	std::cout << "angle: " << angle << '\n';
+	std::cout.flush();
 }
 // Initialze OpenGL perspective matrix
 void GL_Setup(int width, int height) {
-	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 	glMatrixMode(GL_PROJECTION);
 	glViewport(0, 0, width, height);
-	gluPerspective(100, (float) width / height, .1, 100);
+	gluPerspective(45, (float) width / height, .1, 100);
 	glMatrixMode(GL_MODELVIEW);
 }
-
 
 // Initialize GLUT and start main loop
 int main(int argc, char** argv) {
